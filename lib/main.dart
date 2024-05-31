489262dec09d593c37170be373e705a5bfe85af2
Skill Sage <ardevinde@gmail.com>
2024-05-31 23:28:08 +0300

    fixed project not running after adding firebase

--- lib/main.dart
+++ lib/main.dart
@@ -6,10 +6,7 @@ import 'package:navigation/features/auth/presentation/pages/home_page.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 
-Future<void> main() async {
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
+void main(){
   runApp(MyApp());
 }
 
