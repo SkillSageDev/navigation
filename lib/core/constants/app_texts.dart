e37e515e4346edf06261d2940379dabc7c11468a
Skill Sage <ardevinde@gmail.com>
2024-07-21 04:37:15 +0300

    feat(constants): add app_texts.dart for onboarding texts
    Added app_texts.dart to the constants folder containing title and subtitle texts for the onboarding page.

---
+++ lib/core/constants/app_texts.dart
@@ -0,0 +1,16 @@
+class AppTexts{
+  const AppTexts();
+  _OnBoarding get onBoarding => const _OnBoarding();
+}
+
+class _OnBoarding{
+  const _OnBoarding();
+  String get title1 => "Counter Program";
+  String get subtitle1 => "Introducing a user-friendly tool designed to efficiently help you count anything you want.";
+
+  String get title2 => "Make New Friends";
+  String get subtitle2 => "Explore the app to expand your social circle and make new connections.";
+
+  String get title3 => "Count With Friends";
+  String get subtitle3 => "Collaborate with your friends to improve the speed and accuracy of counting tasks.";
+}
