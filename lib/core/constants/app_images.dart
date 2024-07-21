1d9c9191a3c572c0474c455973734f491c45f84d
Skill Sage <ardevinde@gmail.com>
2024-07-21 04:29:39 +0300

    feat(app_images.dart): Add onbaording images path

--- lib/core/constants/app_images.dart
+++ lib/core/constants/app_images.dart
@@ -2,4 +2,7 @@ class AppImages{
   const AppImages();
   String get logo => "assets/images/logo.png";
   String get logoSplash => "assets/images/logo-splash.png";
+  String get onboarding1 => "assets/images/onboarding1.png";
+  String get onboarding2 => "assets/images/onboarding2.png";
+  String get onboarding3 => "assets/images/onboarding3.png";
 }
