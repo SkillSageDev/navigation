7b1007c9578d0d61f9fd9f58f299ed589c2eebb5
Skill Sage <ardevinde@gmail.com>
2024-07-21 05:19:08 +0300

    feat(resources.dart): Create an object of AppTexts in resources.dart
    Created an object of AppTexts in resources.dart to centralize and manage text constants for the onboarding page.
    This improves the organization of constants and makes it easier to maintain and update text content in the future.

--- lib/core/resources/Resources.dart
+++ lib/core/resources/Resources.dart
@@ -3,6 +3,7 @@ import 'package:navigation/config/theme/app_sizes.dart';
 import 'package:navigation/config/theme/app_theme.dart';
 import 'package:navigation/core/constants/app_fonts.dart';
 import 'package:navigation/core/constants/app_images.dart';
+import 'package:navigation/core/constants/app_texts.dart';
 import 'package:navigation/core/utils/utils.dart';
 
 class Resources{
@@ -12,4 +13,5 @@ class Resources{
   static const AppImages image = AppImages();
   static const AppFonts font = AppFonts();
   static const Utils utils = Utils();
+  static const AppTexts texts = AppTexts();
 }
