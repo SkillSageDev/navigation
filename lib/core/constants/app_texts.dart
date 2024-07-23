f8e9085776beb975903f347edfa4ff2f0449a2a3
Skill Sage <ardevinde@gmail.com>
2024-07-23 19:43:23 +0300

    feat(app_texts.dart): Modify subtitle1

--- lib/core/constants/app_texts.dart
+++ lib/core/constants/app_texts.dart
@@ -6,7 +6,7 @@ class AppTexts{
 class _OnBoarding{
   const _OnBoarding();
   String get title1 => "Counter Program";
-  String get subtitle1 => "Introducing a user-friendly tool designed to efficiently help you count anything you want.";
+  String get subtitle1 => "Introducing an intuitive tool for effortless and accurate counting.";
 
   String get title2 => "Make New Friends";
   String get subtitle2 => "Explore the app to expand your social circle and make new connections.";
