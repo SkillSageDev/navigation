cdec93495cadaeb52a5386d7e0aee505b58d9f65
Skill Sage <ardevinde@gmail.com>
2024-07-23 19:46:42 +0300

    feat(main.dart): Add go_router

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 // ignore_for_file: prefer_const_constructors
 
 import 'package:flutter/material.dart';
-import 'package:navigation/core/resources/Resources.dart';
-import 'package:navigation/features/auth/presentation/pages/login_page.dart';
+import 'package:navigation/config/routes/routes.dart';
+import 'package:navigation/core/resources/resources.dart';
 
 void main(){
   runApp(MyApp());
@@ -13,13 +13,12 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp.router(
       debugShowCheckedModeBanner: false,
       theme: Resources.theme.lightTheme,
       darkTheme: Resources.theme.darkTheme,
       themeMode: ThemeMode.system,
-      home: Login(),
-      // routes: {Admin.id: (context) => Admin()},
+      routerConfig: router,
     );
   }
 }
