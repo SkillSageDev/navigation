68b88dff16446d4e0466340c331e93f1048f5a35
Skill Sage <ardevinde@gmail.com>
2024-08-13 20:02:28 +0300

    feat(app_images.dart): Add google and facbook images

--- lib/core/constants/app_images.dart
+++ lib/core/constants/app_images.dart
@@ -1,8 +1,11 @@
-class AppImages{
+class AppImages {
   const AppImages();
-  String get logo => "assets/images/logo.png";
+  String get logoLight => "assets/images/logo-light.png";
+  String get logoDark => "assets/images/logo.png";
   String get logoSplash => "assets/images/logo-splash.png";
   String get onboarding1 => "assets/images/onboarding1.png";
   String get onboarding2 => "assets/images/onboarding2.png";
   String get onboarding3 => "assets/images/onboarding3.png";
+  String get google => "assets/images/google.png";
+  String get facebook => "assets/images/facebook.png";
 }
